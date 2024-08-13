4a7663c42a694cc381e9a35b74cc4fd8d9eb8e65
Cappu123 <cappyalemayehu12@gmail.com>
2024-08-13 20:19:28 +0000

    set js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -4,9 +4,12 @@
  * @return: summ of all student ids
  */
 
-export default function(students) {
+export default function getStudentIdsSum(students) {
   if (students instanceof Array) {
-    return students.reduce((student) => sum(student.id));
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
   }
-  return [];
+  return 0;
 }
